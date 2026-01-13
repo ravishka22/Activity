ced5c2868eb28541e78ad7919256f9446bbc3e17
Ravishka Bahshitha <contact.ravishka@gmail.com>
2026-01-13 13:04:55 +0530

    Add activity lifecycle logging to HomeActivity

--- app/src/main/java/lk/codehive/ravishka/activity/activity/HomeActivity.java
+++ app/src/main/java/lk/codehive/ravishka/activity/activity/HomeActivity.java
@@ -1,7 +1,6 @@
 package lk.codehive.ravishka.activity.activity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,6 +12,7 @@ import lk.codehive.ravishka.activity.log.LogWriter;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private Button homeBtn;
     public HomeActivity(){
         super(R.layout.activity_home);
     }
@@ -21,7 +21,26 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Button homeBtn = findViewById(R.id.homeBtn);
+        homeBtn = findViewById(R.id.homeBtn);
+        LogWriter.writeInfoLog(this,"onCreate() - 01");
+
+    }
+
+    private void printLogStatement(){
+//        System.out.println("INFO: My name is Ravishka");
+        LogWriter.writeInfoLog(this,"My name is Ravishka");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        LogWriter.writeInfoLog(this,"onStart() - 02");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        LogWriter.writeInfoLog(this,"onResume() - 03");
         homeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,8 +49,21 @@ public class HomeActivity extends AppCompatActivity {
         });
     }
 
-    private void printLogStatement(){
-//        System.out.println("INFO: My name is Ravishka");
-        LogWriter.writeInfoLog(this,"My name is Ravishka");
+    @Override
+    protected void onPause() {
+        super.onPause();
+        LogWriter.writeInfoLog(this,"onPause() - 04");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        LogWriter.writeInfoLog(this,"onStop() - 05");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        LogWriter.writeInfoLog(this,"onDestroy() - 06");
     }
 }
